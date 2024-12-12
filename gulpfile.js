3f7113f760fd926097059800cbdc4f587ceb919b
annaleoni <leoni13anna@gmail.com>
2024-12-12 16:50:28 -0700

    update portfolio scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 const uglify = require('gulp-uglify');
-const sass = require('gulp-sass');
+const dartSass = require('gulp-dart-sass'); // Use Dart Sass
 const wait = require('gulp-wait');
-const babel = require('gulp-babel');;
+const babel = require('gulp-babel');
 const rename = require('gulp-rename');
 
+// Scripts task
 gulp.task('scripts', function() {
     return gulp.src('./js/scripts.js')
-        .pipe(plumber(plumber({
+        .pipe(plumber({
             errorHandler: function (err) {
                 console.log(err);
                 this.emit('end');
             }
-        })))
+        }))
         .pipe(babel({
-          presets: [['@babel/env', {modules:false}]]
+            presets: [['@babel/env', {modules: false}]]
         }))
         .pipe(uglify({
             output: {
@@ -26,14 +27,21 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('./js'));
 });
 
+// Styles task
 gulp.task('styles', function () {
     return gulp.src('./scss/styles.scss')
         .pipe(wait(250))
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(dartSass({outputStyle: 'compressed'}).on('error', dartSass.logError)) // Use Dart Sass here
         .pipe(gulp.dest('./css'));
 });
 
+// Watch task
 gulp.task('watch', function() {
-    gulp.watch('./js/scripts.js', gulp.series('scripts'));
-    gulp.watch('./scss/styles.scss', gulp.series('styles'));
+    console.log('Watching for changes...');
+    gulp.watch('./js/scripts.js', gulp.series('scripts')).on('change', function(path) {
+        console.log(`File ${path} was changed`);
+    });
+    gulp.watch('./scss/styles.scss', gulp.series('styles')).on('change', function(path) {
+        console.log(`File ${path} was changed`);
+    });
 });
